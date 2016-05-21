76675616a343c7fb470b5f071cb24600ceb99175
CockDeveloper <COCK.Developer@gmail.com>
2016-05-21 08:13:36 +0700

    * Clone    - Via https://dzone.com/articles/junit-tutorial-beginners    - File: JunitTest.zip
    * Guide:
        -File: step create_build_run Maven Project.txt (text)
    
    * Project:
        -Folder: Calculation

---
+++ Calculation/src/main/java/org/j2eedev/calc/App.java
@@ -0,0 +1,13 @@
+package org.j2eedev.calc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
